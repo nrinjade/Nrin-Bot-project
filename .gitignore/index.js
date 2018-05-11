a8536292c979e0f09e02b5ab8253804f66d4c066
nrinjade <39170643+nrinjade@users.noreply.github.com>
2018-05-11 23:01:56 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -20,30 +20,12 @@ client.on ("ready",() => {
 client.on("message", message =>{
 
     if(message.content === prefix + "version"){
-        message.reply ("Version actuel:0.0.0.1 Alpha 2de Partie");
+        message.reply ("Version actuelle:0.0.0.1 Alpha 2de Partie");
         console.log('Nrinjade III dit la version');
-
-    if(message.content === prefix + "ver"){
-       message.reply ("Version actuel:0.0.0.1 Alpha 2de Partie");
-       console.log('Nrinjade III dit la version')
-
-    if(message.content === prefix + "v"){
-        message.reply ("Version actuel:0.0.0.1 Alpha 2de Partie");
-        console.log('Nrinjade III dit la version')
-    }
 
     if(message.content === prefix + "mappe"){
         message.reply ("C'est la Caaarte!" + " " + "https://cdn.discordapp.com/attachments/436489984854196235/444571876627578881/Mappen_da_Pinter.png");
         console.log('Nrinjade III donne la carte');
-
-    if(message.content === prefix + "map"){
-        message.reply ("C'est la Caaarte!" + " " + "https://cdn.discordapp.com/attachments/436489984854196235/444571876627578881/Mappen_da_Pinter.png");
-        console.log('Nrinjade III donne la carte');
-
-    if(message.content === prefix + "carte"){
-        message.reply ("C'est la Caaarte!" + " " + "https://cdn.discordapp.com/attachments/436489984854196235/444571876627578881/Mappen_da_Pinter.png");
-        console.log('Nrinjade III donne la carte');
-    }
 
     if(message.content === prefix + "legion"){
         var legion_embed = new Discord.RichEmbed()
@@ -67,27 +49,6 @@ client.on("message", message =>{
     }
 
     if(message.content === prefix + "provincesliste"){
-        var pl_embed = new Discord.RichEmbed()
-        .setColor("#610B5E")
-        .setTitle("Liste des provinces du Kastelpônten")
-        .addField("Lisieux","Wixtexio (Normandie)")
-        .addField("Caen","Wixtexio (Normandie)")
-        .addField("Bruxelles","CreeperDiamant (Les Etoiles)")  
-        .addField("Anvers","CreeperDiamant (Les Etoiles)")
-        .addField("Brabant","CreeperDiamant (Les Etoiles)")
-        .addField("Liège","CreeperDiamant (Les Etoiles)")
-        .addField("Wallon","CreeperDiamant (Les Etoiles)")
-        .addField("France","CreeperDiamant (Les Etoiles)")
-        .addField("Allemagne","CreeperDiamant (Les Etoiles)")
-        .addField("Russie","CreeperDiamant (Les Etoiles)")
-        .addField("Météore","CreeperDiamant (Les Etoiles)")
-        .addField("L'océan","CreeperDiamant (Les Etoiles)")
-        .addField("Netince","Netaec (Netince)")
-        .setFooter("Sielte, L&D, Nrinjade, Nrscist: --Menu d'aide-- --Depuis 2018--")
-        message.channel.sendMessage(pl_embed);
-        console.log("Nrinjade III récite la liste des noms des légionnaires")
-
-    if(message.content === prefix + "pl"){
         var pl_embed = new Discord.RichEmbed()
         .setColor("#610B5E")
         .setTitle("Liste des provinces du Kastelpônten")
@@ -166,7 +127,7 @@ client.on("message", message =>{
         message.channel.sendMessage(help_carte_embed);
         console.log("Nrinjade III récite la page d'aide de la commande >=carte")
   
-    if(message.content === prefix + "help"){
+    if(message.content === prefix + "aide"){
         var help_embed = new Discord.RichEmbed()
         .setColor("#610B5E")
         .setTitle("Page d'aide de Nrinbot")
@@ -181,21 +142,4 @@ client.on("message", message =>{
         .setFooter("Sielte, L&D, Nrinjade, Nrscist: --Menu d'aide-- --Depuis 2018--")
         message.channel.sendMessage(help_embed);
         console.log("Nrinjade III récite la page d'aide")
-
-    if(message.content === prefix + "h"){
-        var help_embed = new Discord.RichEmbed()
-        .setColor("#610B5E")
-        .setTitle("Page d'aide de Nrinbot")
-        .setDescription("Vous avez ma liste des commandes")
-        .addField(">=aide", "Affiche la page d'aide")
-        .addField(">=aide <nom de la commande>", "Affiche la page d'aide spécifique à la commande (en développement!!!)")
-        .addField(">=version", "Affiche la version du bot")
-        .addField(">=legion", "Donne la liste des membres de la Légion")
-        .addField(">=nrinjade", "Donne des portraits de Nrinjade,(en développement!!!)")
-        .addField(">=provincesliste","Affiche la liste des provinces du Kastelpônten,")
-        .addField(">=carte", "Affiche la carte")
-        .setFooter("Sielte, L&D, Nrinjade, Nrscist: --Menu d'aide-- --Depuis 2018--")
-        message.channel.sendMessage(help_embed);
-        console.log("Nrinjade III récite la page d'aide")
-    
-    }}}}}}}}}}}}}});
+    }}}}}}}}}});
